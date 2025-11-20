601e5e6a4546e6ce51b5c2f1daed6b63ae9a13ba
kyuwon <109644331+hakyuwon@users.noreply.github.com>
2025-11-21 01:33:56 +0900

    [Bronze V] Title: 새싹, Time: 92 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+
+class Main {
+
+	public static void main(String[] args) throws IOException {
+		StringBuilder sb = new StringBuilder();
+        sb.append("         ,r'\"7"+"\n");
+        sb.append("r`-_   ,'  ,/"+"\n");
+        sb.append(" \\. \". L_r'"+"\n");
+        sb.append("   `~\\/"+"\n");
+        sb.append("      |"+"\n");
+        sb.append("      |"+"\n");
+
+        System.out.println(sb);
+
+	}
+}
