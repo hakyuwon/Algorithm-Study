272572e643f3773170b34f183e413413b5227977
kyuwon <109644331+hakyuwon@users.noreply.github.com>
2026-05-29 01:16:47 +0900

    [level 1] Title: 폰켓몬, Time: 3.21 ms, Memory: 64.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        HashMap<Integer, Integer> mon = new HashMap<>();
+        for(int i = 0; i<nums.length; i++){
+            mon.put(nums[i], mon.getOrDefault(nums[i],0)+1);
+        }
+        
+        if(mon.size()<nums.length/2){
+            return mon.size();
+        }
+        else if(mon.size()>=nums.length/2){
+            return nums.length/2;
+        }
+        return 0;
+    }
+}
